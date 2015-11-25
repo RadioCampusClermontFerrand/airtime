74619b76616e2fc6122cc6dbca2037fe551c1c5a
Duncan Sommerville <duncan.sommerville@gmail.com>
2015-11-25 10:11:21 -0500

    More robust approach to Rotations

--- airtime_mvc/application/common/Rotation.php
+++ airtime_mvc/application/common/Rotation.php
@@ -15,49 +15,91 @@ class Rotation {
     private $_history;
 
     /**
-     * @var array internal rotation tracks array
+     * @var CcFiles[] internal rotation tracks array
      */
     private $_tracks = array();
 
+    private $_filters = array();
+
+    private $_timeToFill;
+
     /**
+     * Rotation constructor.
      *
-     * @return $this self, for chaining
+     * @param string|null $startTime string representation of the start time of the next scheduled track.
+     *                               If set, only add as many tracks as will fit in the time between now and
+     *                               $startTime to the internal tracks array
      */
-    public function addCriteria() {
-        // TODO
-        return $this;
+    public function __construct($startTime = null) {
+        $this->_history = $this->_getHistory();
+        $this->_timeToFill = is_null($startTime) ? self::HISTORY_LOOKUP_SECONDS
+            : min(self::HISTORY_LOOKUP_SECONDS, (strtotime($startTime) - time()) / 1000);
     }
 
     /**
-     * Create an array of CcFiles objects based on added criteria to fill up to an hour of unscheduled time.
+     * Fills in any remaining time in the rotation
      *
-     * @param string $startTime string representation of the start time of the next scheduled track.
-     *                          If set, only add as many tracks as will fit in the time between now and
-     *                          $startTime to the internal tracks array
      * @return $this self, for chaining
      */
-    public function build($startTime = null) {
-        $this->_history = $this->_getHistory();
-        $timeToFill = is_null($startTime) ? self::HISTORY_LOOKUP_SECONDS  // Fill up to HISTORY_LOOKUP_SECONDS seconds
-                                          : min(self::HISTORY_LOOKUP_SECONDS, (strtotime($startTime) - time()) / 1000);
-        while ($timeToFill > 0) {
-            $track = $this->_getSuitableTrack($timeToFill);
-            if (!$track) break;
-            $timeToFill -= Application_Common_DateHelper::playlistTimeToSeconds($track->getDbLength());
+    public function build() {
+        // Get a PropelObjectCollection of all suitable tracks so we only have to go to the database once
+        /** @var CcFiles[] $suitableTracks */
+        $suitableTracks = $this->_getSuitableTracks($this->_timeToFill)->getData();
+        while ($this->_timeToFill > 0) {
+            while (!empty($suitableTracks)) {
+                $key = array_rand($suitableTracks);
+                $track = $suitableTracks[$key];
+                if (Application_Common_DateHelper::playlistTimeToSeconds($track->getDbLength()) > $this->_timeToFill) {
+                    array_splice($suitableTracks, $key, 1);
+                } else {
+                    break;
+                }
+            }
+            if (!isset($track) || !$track) break;
+            $this->_timeToFill -= Application_Common_DateHelper::playlistTimeToSeconds($track->getDbLength());
             $this->_tracks[] = $track;
-            // TODO: pop tracks off the history to keep it as close to HISTORY_LOOKUP_SECONDS as possible?
-            $this->_history->prepend($track);  // Add each track to history so our heuristics use it for future tracks
+            array_splice($suitableTracks, $key, 1);
         }
 
         return $this;
     }
 
     /**
+     *
+     * @param string $column
+     * @param mixed $value
+     * @param string $comparison
+     *
+     * @return $this self, for chaining
+     */
+    public function addFilter($column, $value, $comparison = Criteria::EQUAL) {
+        $filter = new stdClass();
+        $filter->column = $column;
+        $filter->value = $value;
+        $filter->comparison = $comparison;
+        $this->_filters[] = $filter;
+        return $this;
+    }
+
+    /**
+     * Include the given track in the rotation.
+     *
+     * @param CcFiles $track the track to include
+     *
+     * @return $this self, for chaining
+     */
+    public function includeTrack(CcFiles $track) {
+        $length = Application_Common_DateHelper::playlistTimeToSeconds($track->getDbLength());
+        if ($length < $this->_timeToFill) {
+            Logging::warn("Couldn't include track $track; track length is greater than remaining rotation length");
+            $this->_timeToFill -= $length;
+            $this->_tracks[] = $track;
+        }
+        return $this;
+    }
+
+    /**
      * Schedule the internal tracks array.
-     *
-     * TODO: should this function call build() if _tracks is empty?
-     *
-     * @see Rotation::build()
      */
     public function schedule() {
         $future = $now = new DateTime(null, new DateTimeZone("UTC"));
@@ -67,27 +109,43 @@ class Rotation {
         $currentShowInstance = count($shows['currentShow']) > 0 ? $shows['currentShow']['instance_id'] : null;
 
         if ($currentShowInstance) {
-            $this->_scheduleFallbackRotation($currentShowInstance, $this->_tracks);
+            $this->_scheduleFallbackRotation($currentShowInstance);
         }
         // TODO: if there isn't a current show instance, make one...??
     }
 
     /**
+     *
      * @param int $timeToFill
-     * @return CcFiles
+     *
+     * @return PropelObjectCollection
      */
-    private function _getSuitableTrack($timeToFill) {
-        $track = CcFilesQuery::create()
+    private function _getSuitableTracks($timeToFill) {
+        $tracks = CcFilesQuery::create()
             ->filterByDbLength($timeToFill, Criteria::LESS_EQUAL)
-            ->filterByDbId($this->_history->getPrimaryKeys(), Criteria::NOT_IN)
-            ->filterByDbLength('00:00:00', Criteria::GREATER_THAN)
-            // TODO: build and apply rotation heuristics (based on user input?)
-            ->addAscendingOrderByColumn("random()")
-            ->findOne();
-        return $track;
+            ->filterByDbId($this->_getExcludeArray(), Criteria::NOT_IN)
+            ->filterByDbLength('00:00:00', Criteria::GREATER_THAN);
+        foreach ($this->_filters as $filter) {
+            $tracks->filterBy($filter->column, $filter->value, $filter->comparsion);
+        }
+        // TODO: build and apply rotation heuristics (based on user input?)
+        return $tracks->find();
     }
 
     /**
+     *
+     * @return array
+     */
+    private function _getExcludeArray() {
+        $keys = array();
+        foreach ($this->_tracks as $t) {
+            $keys[] = $t->getDbId();
+        }
+        return array_merge($this->_history->getPrimaryKeys(), $keys);
+    }
+
+    /**
+     *
      * @return PropelObjectCollection
      */
     private function _getHistory() {
@@ -95,19 +153,15 @@ class Rotation {
         $history = CcPlayoutHistoryQuery::create()
             ->filterByDbEnds($pastTimestamp, Criteria::GREATER_EQUAL)
             ->find();
-        $files = array();
-        foreach ($history as $item) {
-            $files[] = $item->getDbFileId();
-        }
-        $history = CcFilesQuery::create()->findPks($files);
+        $history = CcFilesQuery::create()->findPks($history->toKeyValue('PrimaryKey', 'DbFileId'));
         return $history;
     }
 
     /**
+     *
      * @param int $showInstance
-     * @param CcFiles[] $tracks
      */
-    private function _scheduleFallbackRotation($showInstance, $tracks) {
+    private function _scheduleFallbackRotation($showInstance) {
         if (!Zend_Session::isStarted()) Zend_Session::start();
         $scheduler = new Application_Model_Scheduler();
         // Ignore user permissions so the fallbacks can be set on non-user (pypo) requests
@@ -120,7 +174,7 @@ class Rotation {
             )
         );
         $mediaItems = array();
-        foreach ($tracks as $track) {
+        foreach ($this->_tracks as $track) {
             $mediaItems[] = array(
                 "id" => $track->getDbId(),
                 "type" => "audioclip"
